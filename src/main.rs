abab46416f26ee232f738778c3eeea295a129275
Pablo Silva <pablodiegoss@hotmail.com>
2021-07-19 15:12:47 -0300

    Add initial structure to read one file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+
+
 fn main() {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+    for line in content.lines() {
+        println!("{}", line);
+    }
     println!("Hello, world!");
 }
