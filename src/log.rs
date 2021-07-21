984ac01b0ecf63830095e153f2938183d1dc6baf
Pablo Silva <pablodiegoss@hotmail.com>
2021-07-20 22:56:52 -0300

    Create a Vec<Log> for a File

---
+++ src/log.rs
@@ -0,0 +1,66 @@
+#[derive(Debug)]
+pub struct Log {
+    pub request_type: String, // http, https, h2
+    pub close_time: String,   // Timestamp in ISO 8601 format
+    pub elb_identifier: String,
+    pub client: String,                // IP:port
+    pub target: String,                // IP:port
+    pub request_processing_time: f32, // in seconds, with millisecond precision for load balancer -> target. Can be -1 if the connection closed
+    pub target_processing_time: f32, // in seconds, with millisecond precision for target -> load balancer. Can be -1 if the connection closed
+    pub response_processing_time: f32, // in seconds, with millisecond precision for load balancer -> client. Can be -1 if the connection closed
+    pub elb_status_code: i32,
+    pub target_status_code: i32,
+    pub received_bytes: i64,    // The size of the request, in bytes.
+    pub sent_bytes: i64,        // The size of the response, in bytes.
+    pub request_string: String, //double quotes. formatted as HTTP method + protocol://host:port/uri + HTTP version eg. "POST https://example.com.br:443/url1/ HTTP/2.0"
+    pub user_agent: String,     // enclosed in double quotes.
+    pub ssl_cipher: String,
+    pub ssl_protocol: String,
+    pub target_group_arn: String,
+    pub x_amazn_trace_id: String,         // enclosed in double quotes.
+    pub sni_domain_name: String,          // enclosed in double quotes.
+    pub cert_arn: String,                 // enclosed in double quotes.
+    pub matched_rule_priority: i32,       // a value from 1 to 50,000, default rule is 0.
+    pub request_creation_time: String,    // ISO 8601 format.
+    pub actions_executed: String,         // enclosed in double quotes
+    pub redirect_url: String,             // enclosed in double quotes
+    pub error_reason: String,             //enclosed in double quotes
+    pub targets_list: String,             //enclosed in double quotes
+    pub targets_status_code_list: String, //enclosed in double quotes
+    pub classification: String,           //enclosed in double quotes
+    pub classification_reason: String,    //enclosed in double quotes
+}
+
+pub fn create_log(mut log_values: Vec<String>) -> Log {
+    Log {
+        request_type: log_values.remove(0),
+        close_time: log_values.remove(0),
+        elb_identifier: log_values.remove(0),
+        client: log_values.remove(0),
+        target: log_values.remove(0),
+        request_processing_time: log_values.remove(0).parse::<f32>().unwrap(),
+        target_processing_time: log_values.remove(0).parse::<f32>().unwrap(),
+        response_processing_time: log_values.remove(0).parse::<f32>().unwrap(),
+        elb_status_code: log_values.remove(0).parse::<i32>().unwrap(),
+        target_status_code: log_values.remove(0).parse::<i32>().unwrap(),
+        received_bytes: log_values.remove(0).parse::<i64>().unwrap(),
+        sent_bytes: log_values.remove(0).parse::<i64>().unwrap(),
+        request_string: log_values.remove(0),
+        user_agent: log_values.remove(0),
+        ssl_cipher: log_values.remove(0),
+        ssl_protocol: log_values.remove(0),
+        target_group_arn: log_values.remove(0),
+        x_amazn_trace_id: log_values.remove(0),
+        sni_domain_name: log_values.remove(0),
+        cert_arn: log_values.remove(0),
+        matched_rule_priority: log_values.remove(0).parse::<i32>().unwrap(),
+        request_creation_time: log_values.remove(0),
+        actions_executed: log_values.remove(0),
+        redirect_url: log_values.remove(0),
+        error_reason: log_values.remove(0),
+        targets_list: log_values.remove(0),
+        targets_status_code_list: log_values.remove(0),
+        classification: log_values.remove(0),
+        classification_reason: log_values.remove(0),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use structopt::StructOpt;
 
+use log::{create_log, Log};
+pub mod log;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
@@ -11,53 +13,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-struct Log {
-    request_type: String, // http, https, h2
-    close_time: String,   // Timestamp in ISO 8601 format
-    elb_identifier: String,
-    client: String,                // IP:port
-    target: String,                // IP:port
-    request_processing_time: f32, // in seconds, with millisecond precision for load balancer -> target. Can be -1 if the connection closed
-    target_processing_time: f32, // in seconds, with millisecond precision for target -> load balancer. Can be -1 if the connection closed
-    response_processing_time: f32, // in seconds, with millisecond precision for load balancer -> client. Can be -1 if the connection closed
-    elb_status_code: i32,
-    target_status_code: i32,
-    received_bytes: i64,    // The size of the request, in bytes.
-    sent_bytes: i64,        // The size of the response, in bytes.
-    request_string: String, //double quotes. formatted as HTTP method + protocol://host:port/uri + HTTP version eg. "POST https://example.com.br:443/url1/ HTTP/2.0"
-    user_agent: String,     // enclosed in double quotes.
-    ssl_cipher: String,
-    ssl_protocol: String,
-    target_group_arn: String,
-    x_amazn_trace_id: String,      // enclosed in double quotes.
-    sni_domain_name: String,       // enclosed in double quotes.
-    cert_arn: String,              // enclosed in double quotes.
-    matched_rule_priority: i32,    // a value from 1 to 50,000, default rule is 0.
-    request_creation_time: String, // ISO 8601 format.
-    actions_executed: String,      // enclosed in double quotes
-    redirect_url: String,          // enclosed in double quotes
-    error_reason: String,          //enclosed in double quotes
-    targets_list: String,           //enclosed in double quotes
-    targets_status_code_list: String,//enclosed in double quotes
-    classification: String,        //enclosed in double quotes
-    classification_reason: String, //enclosed in double quotes
-}
-
 fn main() {
     let args = Cli::from_args();
     let f = BufReader::new(File::open(&args.path).expect("could not read file"));
     let re = Regex::new(r#""([^"])*"|\s"#).unwrap();
-
+    let mut logs: Vec<Log> = Vec::new();
     for line in f.lines() {
-        println!("\n");
-        for value in line
+        let log_values = line
             .unwrap()
             .split_inclusive(&re)
             .map(|x| x.trim().replace('"', ""))
             .filter(|x| x != "")
-            .collect::<Vec<String>>()
-        {
-            println!("{}", value);
-        }
+            .collect::<Vec<String>>();
+        let log = create_log(log_values);
+        logs.push(log);
     }
 }
