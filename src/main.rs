c31c82e42dbcb1d1d11cb6e90e94e89ecc5826c3
Pablo Silva <pablodiegoss@hotmail.com>
2021-07-22 21:33:14 -0300

    Read each file in one thread

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ pub mod log;
 use std::path::PathBuf;
 use std::fs::metadata;
 use walkdir::WalkDir;
+extern crate num_cpus;
+extern crate threadpool;
+use std::thread;
+use threadpool::ThreadPool;
+
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
@@ -57,35 +62,48 @@ fn convert_lines_to_logs(file_path: &PathBuf) -> Vec<Log>{
     }
     logs
 }
-
+use std::sync::{Mutex, Arc};
+use std::time::{Instant};
 fn main() {
-    // let ncpus = num_cpus::get();
-    // let thread_pool = ThreadPool::new(ncpus*2);
+    let ncpus = num_cpus::get();
+    let thread_pool = ThreadPool::new(ncpus*2);
+    let time = Instant::now();
+    println!("Starting - {}", time.elapsed().as_secs());
     // thread_pool.
     let total_results = 10;
     let args = Cli::from_args();
     let mut file_paths = Vec::<PathBuf>::new();
     file_paths = get_log_paths(file_paths, &args.path);
     let mut log_set = HashSet::<UrlCount>::new();
-    
+    let mut all_logs = Arc::new(Mutex::new(Vec::<Vec<Log>>::new()));
     for file in file_paths{
-        
-        let logs = convert_lines_to_logs(&file);
-        
-        for log in logs{
-            let url_info = UrlCount{url:log.request_string, count:1};
-            if log_set.contains(&url_info){
-                let actual_info = log_set.get(&url_info).expect("what");
-                let new_info = UrlCount{url:url_info.url, count:actual_info.count + 1};
-                log_set.replace(new_info);
-            }else{
-                log_set.insert(url_info);
-            }
-        }        
+        let all_logs = Arc::clone(&all_logs);
+        thread_pool.execute(move || {
+                                let thread_logs = convert_lines_to_logs(&file);
+                                all_logs.lock().unwrap().push(thread_logs);
+                                    }
+        );
+        // let logs = convert_lines_to_logs(&file);
+    } 
+    thread_pool.join();
+    let mut logs = Vec::<Log>::new();
+    for mut log_vec in all_logs.lock().unwrap().drain(..){
+        logs.append(&mut log_vec);
     }
+    for log in logs{
+        let url_info = UrlCount{url:log.request_string, count:1};
+        if log_set.contains(&url_info){
+            let actual_info = log_set.get(&url_info).expect("what");
+            let new_info = UrlCount{url:url_info.url, count:actual_info.count + 1};
+            log_set.replace(new_info);
+        }else{
+            log_set.insert(url_info);
+        }
+    }        
     let result:Vec<UrlCount> = sorted(log_set).collect();
     for url_count in &result[result.len()-total_results..]{
         println!("Count: {} - URL:{} ", url_count.count, url_count.url);
     }
-    
+    println!("Finished in : {}s", time.elapsed().as_secs());
+
 }
