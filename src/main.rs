9709d6e5ea8b2a6ecff70231fb8da3277a1b44f6
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2021-10-29 12:19:22 -0300

    Organize imports

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::time::Instant;
-use structopt::StructOpt;
 pub mod log;
+
+use std::{
+    collections::HashMap,
+    fs,
+    fs::metadata,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+    time::Instant,
+};
+
 use log::Log;
-use std::fs::metadata;
-use std::path::PathBuf;
-use walkdir::WalkDir;
-extern crate num_cpus;
-extern crate threadpool;
-use std::fs;
+
+use num_cpus;
+use structopt::StructOpt;
+use threadpool;
 use threadpool::ThreadPool;
+use walkdir::WalkDir;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -19,7 +24,7 @@ struct Cli {
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
     #[structopt(default_value = "20", short = "l", long = "limit")]
-    limit: i32
+    limit: i32,
 }
 
 fn get_log_paths(mut file_paths: Vec<PathBuf>, path: &PathBuf) -> Vec<PathBuf> {
@@ -51,25 +56,22 @@ fn main() {
     let args = Cli::from_args();
     let log_map = Arc::new(Mutex::new(HashMap::<String, u64>::new()));
     let mut file_paths = Vec::<PathBuf>::new();
-    
+
     println!("-- Checking files to process --");
     file_paths = get_log_paths(file_paths, &args.path);
-    
+
     println!("-- Starting to process {} files --", file_paths.len());
     for path in file_paths {
         let all_logs = Arc::clone(&log_map);
         thread_pool.execute(move || {
             let mut thread_log_map = HashMap::<String, u64>::new();
-            
+
             let f = fs::read_to_string(path).expect("could not read file");
             f.lines().for_each(|line| {
                 let log = line.split(" ").collect::<Log>();
 
                 thread_log_map
-                    .entry(format!(
-                        "{} {}",
-                        log.request_method, log.request_url
-                    ))
+                    .entry(format!("{} {}", log.request_method, log.request_url))
                     .and_modify(|e| *e += 1)
                     .or_insert(1);
             });
@@ -86,7 +88,7 @@ fn main() {
     }
 
     thread_pool.join();
-    
+
     let all_logs = log_map.lock().unwrap();
     let mut count_vec: Vec<(&String, &u64)> = all_logs.iter().collect();
     count_vec.sort_by(|a, b| b.1.cmp(a.1));
@@ -95,11 +97,14 @@ fn main() {
     println!("{:<2} - {:<5} - {:<3}", "#", "Count", "URL");
     for (i, (url, count)) in count_vec.iter().enumerate() {
         if i as i32 == args.limit && args.limit < count_vec.len() as i32 {
-            println!("\n... {} omitted.\nTo see more results use -l or --limit to a desired value.", count_vec.len() - i );
+            println!(
+                "\n... {} omitted.\nTo see more results use -l or --limit to a desired value.",
+                count_vec.len() - i
+            );
             break;
         }
-    
-        println!("{:<2} - {:<5} - {}",i+1, count, url);
+
+        println!("{:<2} - {:<5} - {}", i + 1, count, url);
     }
     println!("\nFinished in : {}ms", time.elapsed().as_millis());
 }
